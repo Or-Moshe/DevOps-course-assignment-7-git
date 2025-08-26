4db5aca0d02ba65f7a06ba5741a612dac6e47516
Or-Moshe <ormoshe2468@gmail.com>
2025-08-26 13:15:06 +0300

    add print hello world

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 current_datetime = datetime.now()
-
+print("Hello World")
 print("Welcome to Git Assignment")
 print("Hello Or Moshe")
 print(current_datetime.strftime("%Y-%m-%d  %H:%M:%S"))
